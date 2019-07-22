e1b68af9d6686807fd54fe47ad4699aa13ce4f00
Cédric Dupont <lunarok@gmail.com>
2019-07-22 10:21:33 +0200

    Update compliancyv4.sh

--- compliancyv4.sh
+++ compliancyv4.sh
@@ -7,7 +7,7 @@ USER="lunarok"
 #Clone or pull latest commit if repo exist
 if [ ! -d "$ID" ]; then
     echo "Git clone repo"
-    git clone https://github.com/$USER/$REPO $ID
+    git clone git@github.com:$USER/$REPO.git $ID
     cd $ID
   else
     cd $ID
