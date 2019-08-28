62af57421ee2171926d3975557af16958692d8ee
Cédric Dupont <lunarok@gmail.com>
2019-08-28 14:44:38 +0200

    Update compliancyv4.sh

--- compliancyv4.sh
+++ compliancyv4.sh
@@ -31,6 +31,15 @@ bash ../fontawesome4to5.sh -d desktop -e js
 echo "Move to jeeObject"
 sed -i "s/object::all()/jeeObject::all()/g" desktop/php/${ID}.php
 
+#Icone link 
+echo "Move icone link"
+sed -i "s/docs\/images/plugin_info/g" desktop/php/${ID}.php
+
+#Remove old doc directories
+echo "Remove old doc directories"
+rm -rf docs/
+rm -rf doc/
+
 #Page équipement conforme
 echo "Check eqlogic page"
 grep -q in_searchEqlogic desktop/php/$ID.php
@@ -39,6 +48,16 @@ then
     echo "eqlogic page ok"
 else
     echo "Please update manualy eqlogic page"
+fi
+
+#Search for python2 to migrate to python3
+echo "Check python2 in use"
+grep -q "python " core/class/$ID.class.php
+if [ $? -eq 0 ]
+then
+    echo "No python2 found"
+else
+    echo "Python 2 is found, please update"
 fi
 
 #Présence de widgets
@@ -52,7 +71,7 @@ else
 fi
 
 #Push changes
-git commit -a -m "Compliancy v4"
+git commit -a -m "Compliancy script v4, icon link ..."
 git push
 
 cd ..
